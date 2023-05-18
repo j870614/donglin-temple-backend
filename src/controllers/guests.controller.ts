269b3a6dd32ff421420034d9f28b5202efb01d1a
Hironaka <j870614@gmial.com>
2023-05-18 14:05:14 +0800

    refactor: 💡 取得四眾所有個資 API 用 Tsoa 重構

--- src/controllers/guests.controller.ts
+++ src/controllers/guests.controller.ts
@@ -1,198 +1,207 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable class-methods-use-this */
-import { Response, NextFunction } from "express";
-import { StatusCodes } from "http-status-codes";
 
-import { Get, Queries, Route, SuccessResponse, Tags } from "tsoa";
-import { responseSuccess, responseSuccessData } from "../utils/responseSuccess";
-import { GuestRequest } from "../models/guests.model";
+import { StatusCodes } from "http-status-codes";
+import {
+  BodyProp,
+  Controller,
+  Example,
+  Get,
+  Post,
+  Query,
+  Res,
+  Response,
+  Route,
+  Security,
+  SuccessResponse,
+  Tags
+} from "tsoa";
+import { managers } from "@prisma/client";
+import { TsoaResponse } from "src/utils/ErrorResponse";
 import { prisma } from "../configs/prismaClient";
-import { appError } from "../utils/appError";
 
 @Tags("Guest")
-@Route("/guests")
-export class GuestsController {
+@Route("/api/guests")
+export class GuestsController extends Controller {
+  /**
+   * 取得所有四眾個資
+   * @param order 正序("asc") / 倒序("desc")
+   * @param take 顯示數量
+   * @param skip 略過數量
+   */
   @Get()
-  @SuccessResponse(StatusCodes.OK, "OK")
+  @SuccessResponse(StatusCodes.OK, "查詢成功")
   public async getAll(
-    @Queries() requestQuery: { order: string; take: number; skip: number }
-  ) {
-    const { order, take, skip } = requestQuery;
-    const orderOption = order === "asc" ? "asc" : "desc";
-    const takeOption = Number(take || 100);
-    const skipOption = Number(skip || 0);
+    @Query() order: 'asc' | "desc" = 'desc',
+    @Query() take = 100,
+    @Query() skip = 0
+    ) {
 
-    try {
-      const allUsers = await prisma.users.findMany({
-        orderBy: { Id: orderOption },
-        take: takeOption,
-        skip: skipOption
-      });
+    const allUsers = await prisma.users.findMany({
+      orderBy: { Id: order },
+      take,
+      skip
+    });
 
       return { status: true, allUsers };
-    } catch (error: unknown) {
-      if (error instanceof Error) throw error;
-      throw new Error("");
-    }
   }
 
-  public getGuest = async (req: GuestRequest, res: Response) => {
-    // @swagger.tags = ['User']
-    try {
-      const guestId = Number(req.params.id);
-      const guest = await prisma.users.findUnique({
-        where: {
-          Id: guestId
-        }
-      });
-      responseSuccess(res, StatusCodes.OK, guest);
-    } catch (error: unknown) {
-      if (error instanceof Error) throw error;
-    }
-  };
+//   public getGuest = async (req: GuestRequest, res: Response) => {
+//     // @swagger.tags = ['User']
+//     try {
+//       const guestId = Number(req.params.id);
+//       const guest = await prisma.users.findUnique({
+//         where: {
+//           Id: guestId
+//         }
+//       });
+//       responseSuccess(res, StatusCodes.OK, guest);
+//     } catch (error: unknown) {
+//       if (error instanceof Error) throw error;
+//     }
+//   };
 
-  public createUser = async (
-    req: GuestRequest,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const { IsMonk, Gender } = req.body;
+//   public createUser = async (
+//     req: GuestRequest,
+//     res: Response,
+//     next: NextFunction
+//   ) => {
+//     try {
+//       const { IsMonk, Gender } = req.body;
 
-      if (Gender === undefined) {
-        next(appError(StatusCodes.BAD_REQUEST, "性別未填寫", next));
-        return;
-      }
+//       if (Gender === undefined) {
+//         next(appError(StatusCodes.BAD_REQUEST, "性別未填寫", next));
+//         return;
+//       }
 
-      if (IsMonk === undefined) {
-        next(appError(StatusCodes.BAD_REQUEST, "身分別未填寫", next));
-        return;
-      }
+//       if (IsMonk === undefined) {
+//         next(appError(StatusCodes.BAD_REQUEST, "身分別未填寫", next));
+//         return;
+//       }
 
-      if (IsMonk) {
-        this.checkMonkFields(req, res, next);
-      } else {
-        this.checkBuddhistFields(req, res, next);
-      }
+//       if (IsMonk) {
+//         this.checkMonkFields(req, res, next);
+//       } else {
+//         this.checkBuddhistFields(req, res, next);
+//       }
 
-      const {
-        MobilePrefix,
-        Mobile,
-        Name,
-        DharmaName,
-        MageNickname,
-        LineId,
-        Email,
-        StayIdentity,
-        BirthDate,
-        IdNumber,
-        PassportNumber,
-        BirthPlace,
-        Phone,
-        Ordination,
-        Altar,
-        ShavedMaster,
-        ShavedDate,
-        OrdinationTemple,
-        OrdinationDate,
-        ResidentialTemple,
-        RefugueMaster,
-        RefugueDate,
-        Referrer,
-        ClothType,
-        ClothSize,
-        EmergencyName,
-        EmergencyPhone,
-        Relationship,
-        Expertise,
-        Education,
-        ComeTempleReason,
-        HealthStatus,
-        EatBreakfast,
-        EatLunch,
-        EatDinner,
-        Address,
-        Remarks
-      } = req.body;
-      console.log(typeof IsMonk);
-      const user = await prisma.users.create({
-        data: {
-          MobilePrefix,
-          Mobile,
-          Name,
-          DharmaName,
-          MageNickname,
-          LineId,
-          Email,
-          IsMonk,
-          StayIdentity,
-          Gender,
-          BirthDate,
-          IdNumber,
-          PassportNumber,
-          BirthPlace,
-          Phone,
-          Ordination,
-          Altar,
-          ShavedMaster,
-          ShavedDate,
-          OrdinationTemple,
-          OrdinationDate,
-          ResidentialTemple,
-          RefugueMaster,
-          RefugueDate,
-          Referrer,
-          ClothType,
-          ClothSize,
-          EmergencyName,
-          EmergencyPhone,
-          Relationship,
-          Expertise: undefined,
-          Education,
-          ComeTempleReason,
-          HealthStatus: undefined,
-          EatBreakfast,
-          EatLunch,
-          EatDinner,
-          Address: undefined,
-          Remarks
-        }
-      });
+//       const {
+//         MobilePrefix,
+//         Mobile,
+//         Name,
+//         DharmaName,
+//         MageNickname,
+//         LineId,
+//         Email,
+//         StayIdentity,
+//         BirthDate,
+//         IdNumber,
+//         PassportNumber,
+//         BirthPlace,
+//         Phone,
+//         Ordination,
+//         Altar,
+//         ShavedMaster,
+//         ShavedDate,
+//         OrdinationTemple,
+//         OrdinationDate,
+//         ResidentialTemple,
+//         RefugueMaster,
+//         RefugueDate,
+//         Referrer,
+//         ClothType,
+//         ClothSize,
+//         EmergencyName,
+//         EmergencyPhone,
+//         Relationship,
+//         Expertise,
+//         Education,
+//         ComeTempleReason,
+//         HealthStatus,
+//         EatBreakfast,
+//         EatLunch,
+//         EatDinner,
+//         Address,
+//         Remarks
+//       } = req.body;
+//       console.log(typeof IsMonk);
+//       const user = await prisma.users.create({
+//         data: {
+//           MobilePrefix,
+//           Mobile,
+//           Name,
+//           DharmaName,
+//           MageNickname,
+//           LineId,
+//           Email,
+//           IsMonk,
+//           StayIdentity,
+//           Gender,
+//           BirthDate,
+//           IdNumber,
+//           PassportNumber,
+//           BirthPlace,
+//           Phone,
+//           Ordination,
+//           Altar,
+//           ShavedMaster,
+//           ShavedDate,
+//           OrdinationTemple,
+//           OrdinationDate,
+//           ResidentialTemple,
+//           RefugueMaster,
+//           RefugueDate,
+//           Referrer,
+//           ClothType,
+//           ClothSize,
+//           EmergencyName,
+//           EmergencyPhone,
+//           Relationship,
+//           Expertise: undefined,
+//           Education,
+//           ComeTempleReason,
+//           HealthStatus: undefined,
+//           EatBreakfast,
+//           EatLunch,
+//           EatDinner,
+//           Address: undefined,
+//           Remarks
+//         }
+//       });
 
-      responseSuccess(res, StatusCodes.OK, user);
-    } catch (error: unknown) {
-      if (error instanceof Error) throw error;
-      throw new Error("Unexpected error in guests createUser");
-    }
-  };
+//       responseSuccess(res, StatusCodes.OK, user);
+//     } catch (error: unknown) {
+//       if (error instanceof Error) throw error;
+//       throw new Error("Unexpected error in guests createUser");
+//     }
+//   };
 
-  private checkMonkFields = (
-    req: GuestRequest,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const { DharmaName, ShavedMaster } = req.body;
-    const errMsgArr: string[] = [];
+//   private checkMonkFields = (
+//     req: GuestRequest,
+//     res: Response,
+//     next: NextFunction
+//   ) => {
+//     const { DharmaName, ShavedMaster } = req.body;
+//     const errMsgArr: string[] = [];
 
-    if (!DharmaName) errMsgArr.push("法名");
+//     if (!DharmaName) errMsgArr.push("法名");
 
-    if (!ShavedMaster) errMsgArr.push("剃度師長德號");
+//     if (!ShavedMaster) errMsgArr.push("剃度師長德號");
 
-    if (errMsgArr.length !== 0) {
-      const errMsg = errMsgArr.join("、");
-      next(appError(StatusCodes.BAD_REQUEST, `${errMsg} 未填寫`, next));
-    }
-  };
+//     if (errMsgArr.length !== 0) {
+//       const errMsg = errMsgArr.join("、");
+//       next(appError(StatusCodes.BAD_REQUEST, `${errMsg} 未填寫`, next));
+//     }
+//   };
 
-  private checkBuddhistFields = (
-    req: GuestRequest,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const { Name } = req.body;
+//   private checkBuddhistFields = (
+//     req: GuestRequest,
+//     res: Response,
+//     next: NextFunction
+//   ) => {
+//     const { Name } = req.body;
 
-    if (!Name) {
-      next(appError(StatusCodes.BAD_REQUEST, `俗名未填寫`, next));
-    }
-  };
+//     if (!Name) {
+//       next(appError(StatusCodes.BAD_REQUEST, `俗名未填寫`, next));
+//     }
+//   };
 }
