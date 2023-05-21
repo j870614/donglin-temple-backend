81294e03612d65b5121427bf4e21377d5de3446e
Hironaka <j870614@gmial.com>
2023-05-21 10:39:36 +0800

    refactor: 💡 用 Tsoa 改寫佛七 API

--- src/controllers/buddhaSeven.controller.ts
+++ src/controllers/buddhaSeven.controller.ts
@@ -1,91 +1,123 @@
 /* eslint-disable class-methods-use-this */
-import { Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Path,
+  Res,
+  Response,
+  Route,
+  SuccessResponse,
+  Tags
+} from "tsoa";
 import moment from 'moment';
 
+import { TsoaResponse } from "src/utils/responseTsoaError";
 import { responseSuccess } from "../utils/responseSuccess";
-import { BuddhaSevenRequest } from '../models/buddhaSeven.model';
 import { prisma } from "../configs/prismaClient";
-import { appError } from "../utils/appError";
-// import { config } from "dotenv";
 
-export class BuddhaSevenController {
-  // constructor(private readonly _manager: ManagerDocument) {}
+import { BuddhaSeven } from "../models/buddhaSeven.model";
 
-  public getAllBuddhaSeven = async (req: BuddhaSevenRequest, res: Response, next: NextFunction) => {
-    /**
-     * order 參數表示排列方式，預設是按照 id 由小到大排列
-     * take 參數表示顯示資料數量，佛七預設顯示一年度 36 期的佛七
-     * year 參數表示查詢該年度之佛七
-     */
-    const { order, take, skip ,year } = req.query;
-    const orderOption = order === "asc" || '' ? "asc" : "desc";
-    const takeOption = Number(take || 36);
-    const skipOption = Number(skip || 0);
-    const yearOption = Number(year || moment().year());
-
-    try {
-      const startDate = new Date(`${yearOption}-01-01`);
-      const endDate = new Date(`${yearOption}-12-31`);
-      const buddhaSevenYearly = await prisma.buddha_seven_periods.findMany({
-        orderBy: { Id: orderOption },
-        take: takeOption,
-        skip: skipOption,
-        where: {
-          StarSevenDate: {
-            gte: startDate,
-            lte: endDate,
-          }
+@Tags("Buddha seven - 佛七")
+@Route("/api/buddha-seven")
+export class BuddhaSevenController extends Controller {
+  /**
+   * 取得年度佛七
+   * @param year 查詢該年度之佛七，預設為本年度
+   * @param order 正序("asc") / 倒序("desc")，佛七預設為正序排列
+   * @param take 顯示數量，預設為 36 期佛七
+   * @param skip 略過數量
+   */
+  @Get()
+  @SuccessResponse(StatusCodes.OK, "查詢成功")
+  public async getAllBuddhaSeven (
+    @Query() year = Number(moment().year()),
+    @Query() order: "asc" | "desc" = "asc",
+    @Query() take = 36,
+    @Query() skip = 0,
+  ) {
+    const startDate = new Date(`${year}-01-01`);
+    const endDate = new Date(`${year}-12-31`);
+    const buddhaSevenYearly = await prisma.buddha_seven_periods.findMany({
+      orderBy: { Id: order },
+      take,
+      skip,
+      where: {
+        StartSevenDate: {
+          gte: startDate,
+          lte: endDate,
         }
-      });
-      
-      responseSuccess(res, StatusCodes.OK, buddhaSevenYearly);
-    } catch (error: unknown) {
-      if (error instanceof Error) throw error;
-      throw new Error("Unexpected error in BuddhaSevenController getAllBuddhaSeven");
-    }
-  };
-
-
-  public getBuddhaSeven = async (req: BuddhaSevenRequest, res: Response) => {
-    try {
-      const buddhaSevenId = Number(req.params.id)
-      const buddhaSeven = await prisma.buddha_seven_periods.findUnique({
-        where: {
-          Id:buddhaSevenId
-        },
-      });
-      responseSuccess(res, StatusCodes.OK, buddhaSeven);
-    } catch (error: unknown) {
-      if (error instanceof Error) throw error;
-      throw new Error("Unexpected error in BuddhaSevenController getBuddhaSeven");
-    }
-  };
-
-  public createBuddhaSeven = async (req: BuddhaSevenRequest, res: Response, next: NextFunction ) => {
-    try {
-      
-      const { StarSevenDate, CompleteDate, Remarks } = req.body;
-
-      if (!StarSevenDate || !CompleteDate ) {
-        next(appError(StatusCodes.BAD_REQUEST, '起七日、圓滿日 未填寫', next));
-        return;
       }
+    });
+    
+    return responseSuccess("查詢成功", { buddhaSevenYearly });
+  };
 
-      const buddhaSeven = await prisma.buddha_seven_periods.create({
-        data: {
-          StarSevenDate: new Date(StarSevenDate),
-          CompleteDate: new Date(CompleteDate),
-          Remarks,
-        },
-      })
+  /**
+   * 取得單期佛七
+   * @param id 佛七期數
+   */
+  @Get("{id}")
+  @SuccessResponse(StatusCodes.OK, "查詢成功")
+  @Response(StatusCodes.BAD_REQUEST, "查無佛七期數")
+  public async getBuddhaSeven (
+    @Path() id: number,
+    @Res()
+    errorResponse: TsoaResponse<
+      StatusCodes.BAD_REQUEST,
+      { status: false; message?: string }
+    >
+  ) {
+    const buddhaSeven = await prisma.buddha_seven_periods.findUnique({
+      where: {
+        Id: id
+      },
+    });
 
-      responseSuccess(res, StatusCodes.OK, buddhaSeven);
-
-    } catch (error: unknown) {
-      if (error instanceof Error) throw error;
-      throw new Error("Unexpected error in guests createBuddhaSeven");
+    if (!buddhaSeven) {
+      return errorResponse(StatusCodes.BAD_REQUEST, {
+        status: false,
+        message: "查無此佛七期數"
+      });
     }
-  }
+    
+    return responseSuccess("查詢成功", { buddhaSeven });
+  };
 
+  /**
+   * 新增佛七
+   */
+  @Post()
+  @SuccessResponse(StatusCodes.OK, "新增成功")
+  @Response(StatusCodes.BAD_REQUEST, "新增失敗")
+  public async createBuddhaSeven (
+    @Body() newBuddhaSeven: BuddhaSeven,
+    @Res()
+    errorResponse: TsoaResponse<
+      StatusCodes.BAD_REQUEST,
+      { status: false; message?: string }
+    >
+   ) {
+    const { StartSevenDate, CompleteDate, Remarks } = newBuddhaSeven;
+
+    if (!StartSevenDate || !CompleteDate ) {
+      return errorResponse(StatusCodes.BAD_REQUEST, {
+        status: false,
+        message: `起七日、圓滿日 未填寫`
+      });
+    }
+
+    const buddhaSeven = await prisma.buddha_seven_periods.create({
+      data: {
+        StartSevenDate: new Date(StartSevenDate),
+        CompleteDate: new Date(CompleteDate),
+        Remarks,
+      },
+    })
+
+    return responseSuccess("新增佛七成功", { buddhaSeven });
+  }
 }
--- src/models/buddhaSeven.model.ts
+++ src/models/buddhaSeven.model.ts
@@ -1,21 +1,6 @@
-import { Request } from "express";
-import { ParamsDictionary } from "express-serve-static-core";
-import { buddha_seven_periods } from "@prisma/client";
-
-export type BuddhaSevenParams = ParamsDictionary;
-
-export type BuddhaSevenRequest = Request<
-  ParamsDictionary,
-  object,
-  buddha_seven_periods
->;
-
-// export interface BuddhaSevenRequestBody extends buddha_seven_periods{
-//   Remark: string,
-// }
-
-// export interface BuddhaSevenRequest
-//   extends Request<ParamsDictionary, object, buddha_seven_periods> {
-//   Remark: string,
-// }
-
+export interface BuddhaSeven {
+  Id?: number
+  StartSevenDate: Date
+  CompleteDate: Date
+  Remarks?: string | null
+}
