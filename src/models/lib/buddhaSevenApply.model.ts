495f6623f53ec6ee7a0e372acdbc2bbaafbcd067
Hironaka <j870614@gmial.com>
2023-05-30 15:46:39 +0800

    feat: 🎸 修改 BuddhaSevenApplyRequest，只保留報名佛七所需欄位

--- src/models/lib/buddhaSevenApply.model.ts
+++ src/models/lib/buddhaSevenApply.model.ts
@@ -1,15 +1,10 @@
 export interface BuddhaSevenApplyRequest {
   UserId: number;
-  RoomId?: number;
-  BedStayOrderNumber?: number;
   CheckInDate: Date;
   CheckOutDate: Date;
   CheckInDateBreakfast: boolean;
   CheckInDateLunch: boolean;
   CheckInDateDinner: boolean;
-  CheckInTime?: Date;
-  CheckeInUserId?: number;
-  Status?: string;
   Remarks?: string;
   UpdateUserId: number;
 }
